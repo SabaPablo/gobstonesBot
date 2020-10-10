12da7e2ebf5eeaa85c22caad28e46e7a62ce5fc1
Pablo Saba <psabaliauskas@teco.com.ar>
2020-10-10 04:04:58 -0300

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
-const config = require("./config.json");
+export const DISCORD_TOKEN = process.env["token"];
+
 
 const prefix = "?";
 
@@ -191,4 +192,4 @@ function configuracionOError(message, command){
 }
 
 
-client.login(config.BOT_TOKEN);
+client.login(DISCORD_TOKEN);
